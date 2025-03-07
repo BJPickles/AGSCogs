2a6854c95e5446cfa2241d0de75846535466f716
BJPickles <aegisstreaming@gmail.com>
2025-03-07 14:56:13 +0000

    Update __init__.py

--- agsserverstatus/__init__.py
+++ agsserverstatus/__init__.py
@@ -2,7 +2,7 @@ from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import warning
 import asyncio
 
-from .agsserverstatus import AGSServerStatus
+from .agsserverstatus import agsserverstatus
 
 async def setup(bot: Red):
     cog = AGSServerStatus(bot)
