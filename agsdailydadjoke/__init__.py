e7ba358e53673fd2547acee043e7b5a68697d81c
BJPickles <aegisstreaming@gmail.com>
2026-04-18 23:03:33 +0100

    Update __init__.py

--- agsdailydadjoke/__init__.py
+++ agsdailydadjoke/__init__.py
@@ -1,5 +1,5 @@
 from redbot.core import commands
-from .daily_dadjokes import DailyDadJokes
+from .agsdailydadjoke import DailyDadJokes
 
 async def setup(bot: commands.Bot):
     """Required setup for Red to load the DailyDadJokes cog."""
